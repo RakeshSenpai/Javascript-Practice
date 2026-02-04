71f4fec34e63d07952efdf3b454dc562c4894c5d
Rakesh07778777 <bossrakesh43@gmail.com>
2026-02-04 23:54:41 +0530

    todo

---
+++ todo-list/todo.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('.js-input')
+const addBtn = document.querySelector('.add-btn')
+
+let todoList = [
+    {
+        name: 'Washing Dishes'
+    },
+
+    {
+        name : 'Watching Yotube'
+    }
+]
+
+renderTodo()
+
+function renderTodo(){
+    const container = document.querySelector('.container')
+    const div = document.createElement('div')
+    div.className = 'todo-items'
+    container.appendChild(div)
+    todoList.forEach((items) => {
+        const name = items.name
+        div.textContent = name
+        
+    })
+}
